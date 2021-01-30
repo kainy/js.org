9d3b69b4332e4e19347e63d93b8d672e6a25cdb3
CircleCI <gt@kainy.cn>
2021-01-30 05:34:18 +0000

    pages commit

---
+++ miniprograms/wedding/HunSha/ZhongShiLiFu.js
@@ -0,0 +1,64 @@
+{
+  "options": {
+    "nickName": "中式礼服",
+    "avatar": "http://wx2.sinaimg.cn/sq612/4d6e3e3bgy1ga4mjq7nzqj20h10h1tti.jpg",
+    "bgMusic": "https://api.qq.jsososo.com/song/url?isRedirect=1&id=003I9oST3Aldam",
+    "bgMusicName": "最浪漫的事",
+    "loadingTxt": "时空穿梭中…",
+    "shareTitle": "和$nickname一起时光穿梭",
+    "shareBtnTxt": "微信分享",
+    "hideCatBtn": false,
+    "hideContactBtn": false
+  },
+  "swiperList": [{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4lxssjzcj20rs11ab0q.jpg",
+    "text": "站如松",
+    "swiperItmeClass": "bp-right"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4lxtfn2nj20rs0zne3l.jpg",
+    "text": "坐如钟"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4ostd9m3j20jx0rsb29.jpg",
+    "posterUrl": "https://wx1.sinaimg.cn/mw690/4d6e3e3bgy1ga4p1oc0zzj20ie0idx2r.jpg",
+    "text": "成双入对"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4ov1nel8j20g30rs1i9.jpg",
+    "posterUrl": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4owfklb6j20hk0hkqks.jpg",
+    "text": "玉树临风"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga7301i9uoj20oj18ae82.jpg",
+    "posterUrl": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga733ze7s4j20lq0lq4qp.jpg",
+    "text": "喜"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4mhhqxr0j20g70rr1kx.jpg",
+    "posterUrl": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4mkgqs4xj20el0el17f.jpg",
+    "text": "羞答答的新娘"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4lunvqxjj20rs15o4qr.jpg",
+    "text": "花丛中"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4lu07bd5j20rs15o1kz.jpg",
+    "text": "真香！"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4lveuv8xj20vo18ghdv.jpg",
+    "posterUrl": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4ml93gfrj20xl0xlnpe.jpg",
+    "text": "三人行"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4m03f62sj20rs15ou0u.jpg",
+    "text": "照镜子"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga736l1fv5j20rs15o4o7.jpg",
+    "posterUrl": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga739iu4jvj20rs0rxx6p.jpg",
+    "text": "扶栏"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4lxu8w9pj20rs15o4qp.jpg",
+    "posterUrl": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4mm77f71j20ox0oxnpd.jpg",
+    "text": "深情"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4m043jhsj20rs15o1kx.jpg",
+    "text": "期盼"
+  },{
+    "url": "https://wx1.sinaimg.cn/large/4d6e3e3bgy1ga4m04n97mj20rs15o1kx.jpg",
+    "text": "亲密爱人"
+  }]
+}
